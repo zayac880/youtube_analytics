8d0cdc41bc9792882bedf2044be0f1f2c51121b2
Alex <zayac880880@mail.ru>
2023-05-03 23:29:02 +0300

    Initial commit

---
+++ src/channel.py
@@ -0,0 +1,12 @@
+
+
+class Channel:
+    """Класс для ютуб-канала"""
+
+    def __init__(self, channel_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        print(self.channel_id)
