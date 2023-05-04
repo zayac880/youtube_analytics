6c8118178753b057b4c17f348aed7142057ba490
zayac880 <119949619+zayac880@users.noreply.github.com>
2023-05-04 18:09:01 +0300

    Update channel.py

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,7 @@ import json
 from googleapiclient.discovery import build
 
 # Ключ API для работы с ютуб
-api_key = 'AIzaSyAEaqHuKuD9q5oK4faLZ0BBvwW8RE_Dn08'
+api_key = 'AIzaSyAE........'
 
 class Channel:
     """Класс для ютуб-канала"""
